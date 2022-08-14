6a5964cd4e03a9bea7a6f8498dd17f2bc92601a6
lihawhaw <lihawhaw@163.com>
2022-08-14 14:10:19 +0800

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vitepress'
+import MarkDownItCustomAnchor from './markdown-it-custom-anchor'
+
+
+export default defineConfig({
+  title: '开发配置',
+  description: '',
+  lang: 'zh-CN',
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+  ],
+
+  vue: {
+    reactivityTransform: true
+  },
+
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/lihawhaw/app-config-docs' }
+    ],
+
+    localeLinks: {
+      text: '简体中文',
+      items: []
+    },
+
+    nav: [
+      { text: '指南', link: '/docs/', activeMatch: '/docs/' },
+    ],
+
+    sidebar: {
+      '/docs/': [
+        {
+          text: '指南',
+          items: [
+            {
+              text: '开始',
+              link: '/docs/'
+            },
+          ]
+        },
+      ]
+    }
+  },
+
+  markdown: {
+    // anchor: {
+    //   permalink: renderPermaLink,
+    // },
+    config: (md) => {
+      md.use(MarkDownItCustomAnchor)
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,29 @@
+---
+layout: home
+
+title: 李哈哈
+titleTemplate: 应用程序的配置文档
+
+hero:
+  name: 李哈哈
+  text: 工程化配置文档
+  tagline: 为前端开发提供工程化相关配置
+  actions:
+    - theme: brand
+      text: 开始
+      link: /docs/
+    - theme: alt
+      text: 在 GitHub 上查看
+      link: https://github.com/lihawhaw/app-config-docs
+
+features:
+  - icon:
+    title: 前端
+    details:
+  - icon:
+    title: 后端
+    details:
+  - icon:
+    title: 其他
+    details:
+---
